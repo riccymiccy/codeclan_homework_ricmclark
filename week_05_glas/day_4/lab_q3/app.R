c63b44dc57cde332b7e3a8a7178770b5920195b5
riccymiccy <richard_m_clark@hotmail.co.uk>
2020-04-12 22:17:34 +0100

    lab q3

---
+++ week_05_glas/day_4/lab_q3/app.R
@@ -0,0 +1,61 @@
+library(ggplot2)
+library(CodeClanData)
+library(dplyr)
+library(shiny)
+
+ui <- fluidPage(
+    
+    titlePanel("Reaction Time vs Memory Game"),
+    
+    sidebarLayout(
+        sidebarPanel(
+            radioButtons("colour",
+                         "Colour of points",
+                         choices = c(Blue = "#3891A6", Yellow = "#FDE74C", Red = "#E3655B")
+                         
+            ),
+            
+            
+            sliderInput("trans_p",
+                        label = h3("Transparency of points"),
+                        min = 0, max = 1, value = 0.7),
+            
+            
+            selectInput("shape",
+                        label = h3("Shape of points"),
+                        choices = c("Square" = 15, "Circle" = 16, "Triangle" = 17)
+                        
+            )
+        ),
+        
+        
+        mainPanel(
+            plotOutput("mem_react_chart")
+        )
+        
+    )
+    
+)
+
+
+
+
+
+server <- function(input, output) {
+    
+    
+    
+    output$mem_react_chart <- renderPlot({
+        sstudents_big %>% 
+            ggplot() +
+            aes(x = gender) +
+            geom_bar() 
+    })
+    
+    
+}
+shinyApp(ui = ui, server = server)
+
+
+
+
